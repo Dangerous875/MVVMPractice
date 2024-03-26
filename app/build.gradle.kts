46da042b39792f7be3ae5b4a7cc95752f7bc043c
Dangerous875 <crbarzabal@gmail.com>
2024-03-25 22:51:39 -0300

    Dagger hilt OK :)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    kotlin("kapt")
+    id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
 }
 
@@ -63,9 +63,9 @@ dependencies {
     // Corrutine
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutines")
 
-    val daggerHilt = "2.44"
-    implementation("com.google.dagger:hilt-android:$daggerHilt")
-    kapt("com.google.dagger:hilt-android-compiler:$daggerHilt")
+    //DaggerHilt
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-compiler:2.48")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
